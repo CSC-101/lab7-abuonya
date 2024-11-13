b3b7a2ceaaefa279611d2e10feddd4d8beb5f1f2
annalisebuonya <abuonya@calpoly.edu>
2024-11-12 22:14:39 -0800

    update 3 to task 3

--- command_line.py
+++ command_line.py
@@ -3,18 +3,18 @@ import sys
 # TASK 3
 # DESIGN RECIPE:
     # Purpose of Function: processes the command-line arguments by converting each into a float (skipping any that cannot be converted), adding them all together, and then printing the sum.
-    # Input: "72.2" --> str  #Ouput Given Input: 72.2 --> float
-what the heck am i doing
+    # Input: "72.2" "meow" --> str  #Ouput Given Input: 72.2 --> float
 
 def take_sum_of_numbers():
     sum = 0.0
     number_of_inputs = len(sys.argv)
     for i in range(1,number_of_inputs):
         try:
-            float(i)
-            sum += i
+            temp = float(sys.argv[i])
+            sum += temp
         except ValueError:
-            print(i, "is not a valid input. Skipped.")
+            print(sys.argv[i], "is not a valid input. Skipped.")
+    print(sum)
 
 if __name__ == '__main__':
-    print(sys.argv)
+   take_sum_of_numbers()
